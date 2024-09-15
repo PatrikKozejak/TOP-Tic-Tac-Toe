da76092a26b168db6573eed748a62b9c664a8db4
Patrik Kozejak <patrick.kozejak@gmail.com>
2024-09-15 21:00:41 +0200

    Add win condition check

--- js/script.js
+++ js/script.js
@@ -40,11 +40,34 @@ function Cell() {
   return { addMark, getValue };
 }
 
+function EventEmitter() {
+  const events = {};
+
+  function on(event, listener) {
+    if (!events[event]) {
+      events[event] = [];
+    }
+    events[event].push(listener);
+  }
+
+  function emit(event, data) {
+    if (events[event]) {
+      events[event].forEach((listener) => listener(data));
+    }
+  }
+
+  return {
+    on,
+    emit,
+  };
+}
+
 function GameController(
   playerOneName = "Player One",
   playerTwoName = "Player Two"
 ) {
   const board = Gameboard();
+  const eventEmitter = EventEmitter();
 
   const players = [
     {
@@ -70,14 +93,56 @@ function GameController(
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
+  const playerWon = (player) => {
+    // Check each rows for a winning condition
+    const winningRow = board
+      .getBoard()
+      .find((row) => row.every((cell) => cell.getValue() === player.mark));
+
+    if (winningRow) {
+      eventEmitter.emit("playerWin", { winningRow: winningRow });
+      return true;
+    }
+
+    // Check each columns for a winning condition
+    for (let col = 0; col < 3; col++) {
+      const columnCells = board.getBoard().map((row) => row[col]);
+
+      if (columnCells.every((cell) => cell.getValue() === player.mark)) {
+        eventEmitter.emit("playerWin", { winningRow: columnCells });
+        return true;
+      }
+    }
+
+    // Check both diagonals for winning condition
+    const firstDiagonal = board.getBoard().map((row, index) => row[index]);
+
+    if (firstDiagonal.every((cell) => cell.getValue() === player.mark)) {
+      eventEmitter.emit("playerWin", { winningRow: firstDiagonal });
+      return true;
+    }
+
+    const secondDiagonal = board.getBoard().map((row, index) => row[2 - index]);
+
+    if (firstDiagonal.every((cell) => cell.getValue() === player.mark)) {
+      eventEmitter.emit("playerWin", { winningRow: secondDiagonal });
+      return true;
+    }
+    return false;
+  };
+
   const playRound = (cell) => {
     if (cell.getValue() === 0) {
       board.putMark(cell, getActivePlayer().mark);
-
-      switchPlayerTurn();
-      printNewRound();
+      if (!playerWon(getActivePlayer())) {
+        switchPlayerTurn();
+        printNewRound();
+        eventEmitter.emit("validMove", { cell, player: activePlayer });
+      }
+    } else if (cell.getValue() === activePlayer.mark) {
+      eventEmitter.emit("invalidMove", { cell, player: activePlayer });
     } else {
-      console.log("Cell already occupied!");
+      eventEmitter.emit("occupiedCell", { cell });
     }
   };
 
@@ -88,8 +153,8 @@ function GameController(
     playRound,
     getActivePlayer,
     getBoard: board.getBoard,
-    switchPlayerTurn,
-    printNewRound,
+    on: eventEmitter.on,
+    emit: eventEmitter.emit,
   };
 }
 
@@ -97,6 +162,7 @@ function ScreenController() {
   const game = GameController();
   const playerTurnDiv = document.querySelector(".turn");
   const boardDiv = document.querySelector(".board");
+  const guideDiv = document.querySelector(".guide");
 
   const updateScreen = () => {
     boardDiv.textContent = "";
@@ -119,13 +185,32 @@ function ScreenController() {
   };
 
   function clickHandlerBoard(e) {
-    // Get coords of clicked Cell
-    const selectedCell = [e.target.dataset.row, e.target.dataset.column];
+    const selectedCell =
+      game.getBoard()[e.target.dataset.row][e.target.dataset.column];
 
     if (!selectedCell) return;
-    game.playRound(game.getBoard()[selectedCell[0]][selectedCell[1]]);
-    updateScreen();
+
+    game.playRound(selectedCell);
   }
+  game.on("validMove", () => {
+    updateScreen();
+    guideDiv.textContent = "";
+  });
+
+  game.on("invalidMove", () => {
+    guideDiv.textContent = "You already own this cell! Try another one";
+  });
+  game.on("occupiedCell", () => {
+    guideDiv.textContent = "Cell already occupied! Try another one";
+  });
+
+  game.on("playerWin", ({ winningRow }) => {
+    console.log("playerWon emit");
+    updateScreen();
+    playerTurnDiv.textContent = `${game.getActivePlayer().name} has won!`;
+    guideDiv.textContent = "";
+  });
+
   // Initial render
   updateScreen();
 }
